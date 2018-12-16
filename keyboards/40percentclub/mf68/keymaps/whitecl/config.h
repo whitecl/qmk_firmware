84018f95afb3cd1a787c94701d33fb74ae72584d
Chris White <chris@chrislwhite.com>
2018-12-16 07:07:18 -0700

    Attempt to add a proton-c pin configuration to mf68

---
+++ keyboards/40percentclub/mf68/keymaps/whitecl/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#undef MATRIX_ROWS
+#undef MATRIX_COLS
+
+#undef MATRIX_ROW_PINS
+#undef MATRIX_COL_PINS
+
+#define MATRIX_ROWS 5
+#define MATRIX_COLS 12
+#define MATRIX_ROW_PINS { B8, B13, B14, B15, B9 }
+#define MATRIX_COL_PINS { A8, A7, A6, A5, A4, B0, B1, B2, B3, B4, B5, B6 }
